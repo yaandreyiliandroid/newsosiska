b93052ae8b58c731ad039b25b95305e894d5d0cc
yaandreyiliandroid <133146230+yaandreyiliandroid@users.noreply.github.com>
2023-06-13 11:23:19 +0300

    Create newsosiska.py

---
+++ newsosiska.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import os
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    images = os.listdir('images')
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def sosismem(ctx):
+    sosis = os.listdir('sosis')
+    img_name = random.choice(sosis)
+    with open(f'sosis/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+bot.run("token")
